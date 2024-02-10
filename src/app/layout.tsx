4ab81c82e43b1411aecc332d9d841cab68ff6368
maksim hodasevich <maksim.hodasevich@gmail.com>
2024-02-10 17:32:19 +0100

    fix css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       <Script async defer src="https://buttons.github.io/buttons.js" />
       <Toaster />
       <body className={inter.className}>
-        <main className="bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]">
+        <main className="bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
           {children}
         </main>
       </body>
